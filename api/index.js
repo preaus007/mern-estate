2410cf4b3c2e521f5f024eba29f4abc1ce2604eb
preaus007 <touhedcs@gmail.com>
2024-08-07 02:08:17 +0600

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Conneted to the database");
+  })
+  .catch((error) => {
+    console.log("Error: " + error);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on 3000");
